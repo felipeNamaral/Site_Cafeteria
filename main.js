e2ef709f2e71c4c158b7ca87794dd7621c33965a
Felipe Amaral <fe.nataliamaral@gmail.com>
2024-09-08 22:39:56 -0300

    add js

---
+++ main.js
@@ -0,0 +1,246 @@
+const img = document.querySelectorAll('.img')
+const imgP = document.querySelectorAll('.imgP')
+
+
+img[0].addEventListener('click',() =>{
+    img[0].style.width = '390px';
+    img[0].style.height = '600px';
+    img[0].style.backgroundColor = 'trasparent';
+    img[0].style.filter = 'saturate(100%)';
+    //diminuir outros
+    img[1].style.width = '240px';
+    img[1].style.height = '370px';
+    img[1].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[1].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[2].style.width = '240px';
+    img[2].style.height = '370px';
+    img[2].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[2].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[3].style.width = '240px';
+    img[3].style.height = '370px';
+    img[3].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[3].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[4].style.width = '240px';
+    img[4].style.height = '370px';
+    img[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[4].style.filter = 'saturate(10%)';
+})
+
+img[1].addEventListener('click',() =>{
+    img[1].style.backgroundColor = 'trasparent';
+    img[1].style.width = '390px';
+    img[1].style.height = '600px';
+    img[1].style.filter = 'saturate(100%)';
+    //diminuir outros
+    img[0].style.width = '240px';
+    img[0].style.height = '370px';
+    img[0].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[0].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[2].style.width = '240px';
+    img[2].style.height = '370px';
+    img[2].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[2].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[3].style.width = '240px';
+    img[3].style.height = '370px';
+    img[3].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[3].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[4].style.width = '240px';
+    img[4].style.height = '370px';
+    img[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[4].style.filter = 'saturate(10%)';
+})
+img[2].addEventListener('click',() =>{
+    img[2].style.width = '390px';
+    img[2].style.height = '600px';
+    img[2].style.backgroundColor = 'trasparent';
+    img[2].style.filter = 'saturate(100%)';
+    //diminuir outros
+    img[1].style.width = '240px';
+    img[1].style.height = '370px';
+    img[1].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[1].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[0].style.width = '240px';
+    img[0].style.height = '370px';
+    img[0].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[0].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[3].style.width = '240px';
+    img[3].style.height = '370px';
+    img[3].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[3].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[4].style.width = '240px';
+    img[4].style.height = '370px';
+    img[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[4].style.filter = 'saturate(10%)';
+})
+img[3].addEventListener('click',() =>{
+    img[3].style.width = '390px';
+    img[3].style.height = '600px';
+    img[3].style.backgroundColor = 'trasparent';
+    img[3].style.filter = 'saturate(100%)';
+    //diminuir outros
+    img[4].style.width = '240px';
+    img[4].style.height = '370px';
+    img[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[4].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[2].style.width = '240px';
+    img[2].style.height = '370px';
+    img[2].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[2].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[0].style.width = '240px';
+    img[0].style.height = '370px';
+    img[0].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[0].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[1].style.width = '240px';
+    img[1].style.height = '370px';
+    img[1].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[1].style.filter = 'saturate(10%)';
+
+})
+img[4].addEventListener('click',() =>{
+    img[4].style.width = '390px';
+    img[4].style.height = '600px';
+    img[4].style.backgroundColor = 'trasparent';
+    img[4].style.filter = 'saturate(100%)';
+    //diminuir outros
+    img[3].style.width = '240px';
+    img[3].style.height = '370px';
+    img[3].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[3].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[2].style.width = '240px';
+    img[2].style.height = '370px';
+    img[2].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[2].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[1].style.width = '240px';
+    img[1].style.height = '370px';
+    img[1].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[1].style.filter = 'saturate(10%)';
+    //diminuir outros
+    img[0].style.width = '240px';
+    img[0].style.height = '370px';
+    img[0].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    img[0].style.filter = 'saturate(10%)';
+})
+
+//produtos
+
+imgP[0].addEventListener('click',() =>{
+    imgP[0].style.width = '390px';
+    imgP[0].style.height = '600px';
+    imgP[0].style.backgroundColor = 'trasparent';
+    imgP[0].style.filter = 'saturate(100%)';
+    //diminuir outros
+    imgP[3].style.width = '240px';
+    imgP[3].style.height = '370px';
+    imgP[3].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[3].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[2].style.width = '240px';
+    imgP[2].style.height = '370px';
+    imgP[2].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[2].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[1].style.width = '240px';
+    imgP[1].style.height = '370px';
+    imgP[1].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[1].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[4].style.width = '240px';
+    imgP[4].style.height = '370px';
+    imgP[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[4].style.filter = 'saturate(10%)';
+})
+
+imgP[1].addEventListener('click',() =>{
+    imgP[1].style.width = '390px';
+    imgP[1].style.height = '600px';
+    imgP[1].style.backgroundColor = 'trasparent';
+    imgP[1].style.filter = 'saturate(100%)';
+    //diminuir outros
+    imgP[3].style.width = '240px';
+    imgP[3].style.height = '370px';
+    imgP[3].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[3].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[2].style.width = '240px';
+    imgP[2].style.height = '370px';
+    imgP[2].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[2].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[0].style.width = '240px';
+    imgP[0].style.height = '370px';
+    imgP[0].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[0].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[4].style.width = '240px';
+    imgP[4].style.height = '370px';
+    imgP[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[4].style.filter = 'saturate(10%)';
+})
+
+imgP[2].addEventListener('click',() =>{
+    imgP[2].style.width = '390px';
+    imgP[2].style.height = '600px';
+    imgP[2].style.backgroundColor = 'trasparent';
+    imgP[2].style.filter = 'saturate(100%)';
+    //diminuir outros
+    imgP[3].style.width = '240px';
+    imgP[3].style.height = '370px';
+    imgP[3].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[3].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[0].style.width = '240px';
+    imgP[0].style.height = '370px';
+    imgP[0].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[0].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[1].style.width = '240px';
+    imgP[1].style.height = '370px';
+    imgP[1].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[1].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[4].style.width = '240px';
+    imgP[4].style.height = '370px';
+    imgP[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[4].style.filter = 'saturate(10%)';
+})
+
+imgP[3].addEventListener('click',() =>{
+    imgP[3].style.width = '390px';
+    imgP[3].style.height = '600px';
+    imgP[3].style.backgroundColor = 'trasparent';
+    imgP[3].style.filter = 'saturate(100%)';
+    //diminuir outros
+    imgP[0].style.width = '240px';
+    imgP[0].style.height = '370px';
+    imgP[0].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[0].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[2].style.width = '240px';
+    imgP[2].style.height = '370px';
+    imgP[2].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[2].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[1].style.width = '240px';
+    imgP[1].style.height = '370px';
+    imgP[1].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[1].style.filter = 'saturate(10%)';
+    //diminuir outros
+    imgP[4].style.width = '240px';
+    imgP[4].style.height = '370px';
+    imgP[4].style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+    imgP[4].style.filter = 'saturate(10%)';
+})
